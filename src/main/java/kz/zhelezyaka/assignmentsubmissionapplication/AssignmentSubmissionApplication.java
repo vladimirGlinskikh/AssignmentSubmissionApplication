62307ce0376d06a514e55704364cc3ed5c2f731d
Vladimir Glinskikh <vladimir.v.glinskikh@gmail.com>
2024-04-10 22:22:37 +0300

    Start Project

---
+++ src/main/java/kz/zhelezyaka/assignmentsubmissionapplication/AssignmentSubmissionApplication.java
@@ -0,0 +1,12 @@
+package kz.zhelezyaka.assignmentsubmissionapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AssignmentSubmissionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AssignmentSubmissionApplication.class, args);
+    }
+}
---
+++ src/test/java/kz/zhelezyaka/assignmentsubmissionapplication/AssignmentSubmissionApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.zhelezyaka.assignmentsubmissionapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AssignmentSubmissionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
